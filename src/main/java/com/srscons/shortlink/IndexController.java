09313b95f349dd91688f1c5b8a968e576f1d82f6
SARKHAN RASULLU <sarkhanrasullu@gmail.com>
2025-04-30 22:08:15 +0400

    initial

---
+++ src/main/java/com/srscons/shortlink/IndexController.java
@@ -0,0 +1,21 @@
+package com.srscons.shortlink;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String indexPage() {
+        return "index";
+    }
+
+
+    @RequestMapping("/dashboard")
+    public String dashboardPage() {
+        return "dashboard";
+    }
+
+}
